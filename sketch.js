e64ebe3081681afd93d1c322a5f13f049c4b97b7
L2442 <swastikshandilya11@gmail.com>
2021-02-07 09:57:12 +0530

    Const. bodies

--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
-var box1, pig1;
-var backgroundImg,platform;
+var box1, pig1,chain;
+var backgroundImg,platform,constrainedLog;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -24,6 +25,7 @@ function setup(){
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
 
+
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
@@ -35,7 +37,9 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    constrainedLog = new Log(200,180,180,PI/2);
 
+   chain = new Chain(bird.body, constrainedLog.body);
 }
 
 function draw(){
@@ -61,4 +65,9 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    chain.display();
+    constrainedLog.display();
+
+   // line(bird.body.position.x,bird.body.position.y,constrainedLog.body.position.x,constrainedLog.body.position.y);
 }
